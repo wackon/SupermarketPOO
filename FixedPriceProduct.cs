2963b0425b72d25df51eeb667c2fa35e7982bd03
Wackon <jeac86@gmail.com>
2022-12-22 13:59:38 -0500

    Add project files.

---
+++ FixedPriceProduct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketPOO
+{
+    internal class FixedPriceProduct : Products
+    {
+        public override decimal valueToPay()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
---
+++ Products.cs
@@ -0,0 +1,23 @@
+﻿namespace SupermarketPOO
+{
+    public abstract class Products
+    {
+        public int Id { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal Tax { get; set; }
+
+        public abstract decimal valueToPay();
+
+        public override string ToString()
+        {
+            return $"{Id}Descripción    :{Descripcion}," +
+                $" Price                :{Price}," +
+                $" Tax                  :{Tax},";
+
+        }
+    }
+}
